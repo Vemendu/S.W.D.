70148a70269f353f10ebffc10b8507098318382d
Vladislav <vemendu@gmail.com>
2022-04-30 19:01:21 +0600

    Added getting a user's city using geolocation

--- app/src/main/java/com/example/finalexamweatherapp/MainActivity.java
+++ app/src/main/java/com/example/finalexamweatherapp/MainActivity.java
@@ -1,15 +1,35 @@
 package com.example.finalexamweatherapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -20,10 +40,16 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
+import java.util.Locale;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    FusedLocationProviderClient mFusedLocationClient;
+    TextView latitudeTextView;
+    int PERMISSION_ID = 44;
 
     private EditText user_field;
     private Button main_btn;
@@ -42,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -57,6 +84,12 @@ public class MainActivity extends AppCompatActivity {
         result_info7 = findViewById(R.id.result_info7);
         result_info8 = findViewById(R.id.result_info8);
         result_info9 = findViewById(R.id.result_info9);
+
+        //КОД ДЛЯ ГЕОЛОКА
+        latitudeTextView = findViewById(R.id.latTextView);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
+        //КОД ДЛЯ ГЕОЛОКА
 
         main_btn.setOnClickListener(view -> {
             if(user_field.getText().toString().trim().equals(""))
@@ -76,6 +109,123 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    //НАЧАЛО КОДА ДЛЯ ГЕОЛОКА
+
+    @SuppressLint("MissingPermission")
+    private void getLastLocation() {
+
+        if (checkPermissions()) {
+
+
+            if (isLocationEnabled()) {
+
+
+                mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        Location location = task.getResult();
+                        if (location == null) {
+                            requestNewLocationData();
+                        } else {
+                            String city = "???";
+                            try {
+                                city = getCity(location.getLatitude(), location.getLongitude());
+                            } catch (IOException e) {
+                                System.out.println("OOPS");
+                                e.printStackTrace();
+                            }
+                            latitudeTextView.setText(city + "");
+                        }
+                    }
+                });
+            } else {
+                Toast.makeText(this, "Please turn on" + " your location...", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        } else {
+            requestPermissions();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void requestNewLocationData() {
+
+
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(5);
+        mLocationRequest.setFastestInterval(0);
+        mLocationRequest.setNumUpdates(1);
+
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+    }
+
+    private LocationCallback mLocationCallback = new LocationCallback() {
+
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location mLastLocation = locationResult.getLastLocation();
+            String city = "???";
+            try {
+                city = getCity(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+            } catch (IOException e) {
+                System.out.println("OOPS");
+                e.printStackTrace();
+            }
+            latitudeTextView.setText("City: " + city + "");
+        }
+    };
+
+    private String getCity(double latitude, double longitude) throws IOException {
+
+        Geocoder geocoder = new Geocoder(this, new Locale("en"));
+        List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+        return addresses.get(0).getLocality();
+    }
+
+    private boolean checkPermissions() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ID);
+    }
+
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    @Override
+    public void
+    onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (checkPermissions()) {
+            getLastLocation();
+        }
+    }
+
+    //КОНЕЦ КОДА ДЛЯ ГЕОЛОКА
+
     private class GetURLData extends AsyncTask<String, String , String > {
 
         protected void onPreExecute() {
